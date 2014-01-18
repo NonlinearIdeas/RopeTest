3cfea26886c0b180cf2ec64503afd0449a117c60
James Wucher <james@nlideas4.gateway.2wire.net>
2014-01-18 08:16:05 -0600

    Initial commit.

---
+++ RopeTest/Classes/HelloWorldScene.cpp
@@ -0,0 +1,311 @@
+//
+//  HelloWorldScene.cpp
+//  RopeTest
+//
+//  Created by James Wucher on 1/18/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+#include "Box2DDebugDrawLayer.h"
+
+using namespace cocos2d;
+using namespace CocosDenshion;
+
+#define PTM_RATIO 32
+
+enum {
+   kTagParentNode = 1,
+};
+
+PhysicsSprite::PhysicsSprite()
+: m_pBody(NULL)
+{
+   
+}
+
+void PhysicsSprite::setPhysicsBody(b2Body * body)
+{
+   m_pBody = body;
+}
+
+// this method will only get called if the sprite is batched.
+// return YES if the physics values (angles, position ) changed
+// If you return NO, then nodeToParentTransform won't be called.
+bool PhysicsSprite::isDirty(void)
+{
+   return true;
+}
+
+// returns the transform matrix according the Chipmunk Body values
+CCAffineTransform PhysicsSprite::nodeToParentTransform(void)
+{
+   b2Vec2 pos  = m_pBody->GetPosition();
+   
+   float x = pos.x * PTM_RATIO;
+   float y = pos.y * PTM_RATIO;
+   
+   if ( isIgnoreAnchorPointForPosition() ) {
+      x += m_obAnchorPointInPoints.x;
+      y += m_obAnchorPointInPoints.y;
+   }
+   
+   // Make matrix
+   float radians = m_pBody->GetAngle();
+   float c = cosf(radians);
+   float s = sinf(radians);
+   
+   if( ! m_obAnchorPointInPoints.equals(CCPointZero) ){
+      x += c*-m_obAnchorPointInPoints.x + -s*-m_obAnchorPointInPoints.y;
+      y += s*-m_obAnchorPointInPoints.x + c*-m_obAnchorPointInPoints.y;
+   }
+   
+   // Rot, Translate Matrix
+   m_sTransform = CCAffineTransformMake( c,  s,
+                                        -s,    c,
+                                        x,    y );
+   
+   return m_sTransform;
+}
+
+HelloWorld::HelloWorld()
+{
+   setTouchEnabled( true );
+   setAccelerometerEnabled( true );
+   
+   CCSize s = CCDirector::sharedDirector()->getWinSize();
+   // init physics
+   this->initPhysics();
+   
+   CCSpriteBatchNode *parent = CCSpriteBatchNode::create("blocks.png", 100);
+   m_pSpriteTexture = parent->getTexture();
+   
+   addChild(parent, 0, kTagParentNode);
+   
+   CCLabelTTF *label = CCLabelTTF::create("Tap screen", "Marker Felt", 32);
+   addChild(label, 0);
+   label->setColor(ccc3(0,0,255));
+   label->setPosition(ccp( s.width/2, s.height-50));
+   
+   scheduleUpdate();
+}
+
+HelloWorld::~HelloWorld()
+{
+   delete world;
+   world = NULL;
+   
+   //delete m_debugDraw;
+}
+
+void HelloWorld::initPhysics()
+{
+   
+   CCSize s = CCDirector::sharedDirector()->getWinSize();
+   
+   b2Vec2 gravity;
+   gravity.Set(0.0f, -10.0f);
+   world = new b2World(gravity);
+   
+   // Do we want to let bodies sleep?
+   world->SetAllowSleeping(true);
+   
+   world->SetContinuousPhysics(true);
+   
+   
+   // Define the ground body.
+   b2BodyDef groundBodyDef;
+   groundBodyDef.position.Set(0, 0); // bottom-left corner
+   
+   // Call the body factory which allocates memory for the ground body
+   // from a pool and creates the ground box shape (also from a pool).
+   // The body is also added to the world.
+   b2Body* groundBody = world->CreateBody(&groundBodyDef);
+   m_pGround = groundBody;
+   
+   // Define the ground box shape.
+   b2EdgeShape groundBox;
+   
+   // bottom
+   
+   groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
+   groundBody->CreateFixture(&groundBox,0);
+   
+   // top
+   groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
+   groundBody->CreateFixture(&groundBox,0);
+   
+   // left
+   groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
+   groundBody->CreateFixture(&groundBox,0);
+   
+   // right
+   groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
+   groundBody->CreateFixture(&groundBox,0);
+   
+   addChild(Box2DDebugDrawLayer::create(world));
+}
+
+void HelloWorld::draw()
+{
+   //
+   // IMPORTANT:
+   // This is only for debug purposes
+   // It is recommend to disable it
+   //
+   CCLayer::draw();
+   
+   ccGLEnableVertexAttribs( kCCVertexAttribFlag_Position );
+   
+   kmGLPushMatrix();
+   
+   world->DrawDebugData();
+   
+   kmGLPopMatrix();
+}
+
+void HelloWorld::addNewSpriteAtPosition(CCPoint p)
+{
+   CCLOG("Add sprite %0.2f x %02.f",p.x,p.y);
+   CCNode* parent = getChildByTag(kTagParentNode);
+   
+   //We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
+   //just randomly picking one of the images
+   int idx = (CCRANDOM_0_1() > .5 ? 0:1);
+   int idy = (CCRANDOM_0_1() > .5 ? 0:1);
+   PhysicsSprite *sprite = new PhysicsSprite();
+   sprite->initWithTexture(m_pSpriteTexture, CCRectMake(32 * idx,32 * idy,32,32));
+   sprite->autorelease();
+   
+   parent->addChild(sprite);
+   
+   sprite->setPosition( CCPointMake( p.x, p.y) );
+   sprite->setOpacity(32);
+   
+   // Define the dynamic body.
+   //Set up a 1m squared box in the physics world
+   b2BodyDef bodyDef;
+   bodyDef.type = b2_dynamicBody;
+   bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+   
+   b2Body *body = world->CreateBody(&bodyDef);
+   
+   // Define another box shape for our dynamic body.
+   b2PolygonShape dynamicBox;
+   dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
+   
+   // Define the dynamic body fixture.
+   b2FixtureDef fixtureDef;
+   fixtureDef.shape = &dynamicBox;
+   fixtureDef.density = 1.0f;
+   fixtureDef.friction = 0.3f;
+   body->CreateFixture(&fixtureDef);
+   
+   sprite->setPhysicsBody(body);
+   
+   // Create a rope joint so that the
+   // block will "swing" from the top of the
+   // scene.
+   
+   // Calculate the local position of the
+   // top of screen in the local space
+   // of the ground box.
+   CCSize scrSize = CCDirector::sharedDirector()->getWinSize();
+   b2Vec2 groundWorldPos = b2Vec2((scrSize.width/2)/PTM_RATIO,(scrSize.height)/PTM_RATIO);
+   b2Vec2 groundLocalPos = m_pGround->GetLocalPoint(groundWorldPos);
+   
+   // Now create the main swinging joint.
+   b2RopeJointDef jointDef;
+   jointDef.bodyA = m_pGround;
+   jointDef.bodyB = body;
+   jointDef.localAnchorA = groundLocalPos;
+   jointDef.localAnchorB = b2Vec2(0.0f,0.0f);
+   jointDef.maxLength = (groundWorldPos-body->GetWorldCenter()).Length();
+   jointDef.collideConnected = true;
+   world->CreateJoint(&jointDef);
+   
+   // Now create a second/third rope to "constrain" the swing.
+   // These one will be attached to the side of the screen.
+   b2Vec2 groundWorldPos2 = b2Vec2(0,body->GetWorldCenter().y);
+   b2Vec2 groundLocalPos2 = m_pGround->GetLocalPoint(groundWorldPos2);
+
+   jointDef.localAnchorA = groundLocalPos2;
+   // Setting the length of the side rope...
+   // What we want to do here is use the distance from the center as
+   // the length of the rope.  Then add length to it so that the box
+   // can have at least one full swing which ever side it is on (left or
+   // right half of the scene).
+   float32 distToCenter = (fabs(scrSize.width/2 - (body->GetWorldCenter().x)*PTM_RATIO))/PTM_RATIO;
+   jointDef.maxLength = distToCenter + (scrSize.width/2)/PTM_RATIO;
+   world->CreateJoint(&jointDef);
+   
+   // Now for the last rope, other side of the scene.
+   // Now create a second/third rope to "constrain" the swing.
+   // These one will be attached to the side of the screen.
+   b2Vec2 groundWorldPos3 = b2Vec2((scrSize.width)/PTM_RATIO,body->GetWorldCenter().y);
+   b2Vec2 groundLocalPos3 = m_pGround->GetLocalPoint(groundWorldPos3);
+   jointDef.localAnchorA = groundLocalPos3;
+   world->CreateJoint(&jointDef);
+
+}
+
+
+void HelloWorld::update(float dt)
+{
+   //It is recommended that a fixed time step is used with Box2D for stability
+   //of the simulation, however, we are using a variable time step here.
+   //You need to make an informed choice, the following URL is useful
+   //http://gafferongames.com/game-physics/fix-your-timestep/
+   
+   int velocityIterations = 8;
+   int positionIterations = 1;
+   
+   // Instruct the world to perform a single step of simulation. It is
+   // generally best to keep the time step and iterations fixed.
+   world->Step(dt, velocityIterations, positionIterations);
+   
+   //Iterate over the bodies in the physics world
+   for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+   {
+      if (b->GetUserData() != NULL) {
+         //Synchronize the AtlasSprites position and rotation with the corresponding body
+         CCSprite* myActor = (CCSprite*)b->GetUserData();
+         myActor->setPosition( CCPointMake( b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO) );
+         myActor->setRotation( -1 * CC_RADIANS_TO_DEGREES(b->GetAngle()) );
+      }
+   }
+}
+
+void HelloWorld::ccTouchesEnded(CCSet* touches, CCEvent* event)
+{
+   //Add a new body/atlas sprite at the touched location
+   CCSetIterator it;
+   CCTouch* touch;
+   
+   for( it = touches->begin(); it != touches->end(); it++)
+   {
+      touch = (CCTouch*)(*it);
+      
+      if(!touch)
+         break;
+      
+      CCPoint location = touch->getLocationInView();
+      
+      location = CCDirector::sharedDirector()->convertToGL(location);
+      
+      addNewSpriteAtPosition( location );
+   }
+}
+
+CCScene* HelloWorld::scene()
+{
+   // 'scene' is an autorelease object
+   CCScene *scene = CCScene::create();
+   
+   // add layer as a child to scene
+   CCLayer* layer = new HelloWorld();
+   scene->addChild(layer);
+   layer->release();
+   
+   return scene;
+}
---
+++ RopeTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RopeTestAppController.h
+//  RopeTest
+//
+//  Created by James Wucher on 1/18/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
